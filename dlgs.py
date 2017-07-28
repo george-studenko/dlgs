70c079f511e4966f9b68559db9e67bb584db8d43
George Studenko <gstudenko@voxelgroup.net>
2017-07-28 09:27:14 +0200

    feat: Add dlgs.py initial code

---
+++ dlgs.py
@@ -0,0 +1,40 @@
+def get_bag_of_words(text):
+	from collections import Counter
+    return Counter(text.split())
+	
+def normalize_image(x):
+    """
+    Normalize a list of sample image data in the range of 0 to 1
+    : x: List of image data. 
+    : return: Numpy array of normalize data
+    """
+    normalized_x_min = 0.1
+    normalized_x_max = 0.9
+    grayscale_min = 0
+    grayscale_max = 255
+    
+    normalized_x = normalized_x_min + ( 
+        ( (x - grayscale_min)*
+        (normalized_x_max - normalized_x_min) )/
+        ( grayscale_max - grayscale_min ))       
+    
+    return normalized_x
+	
+def one_hot_encode_sk(x):
+    """   
+    : x: List of sample Labels
+    : return: array of one-hot encoded labels
+    """        
+    from sklearn import preprocessing
+    
+    labels_vecs = preprocessing.LabelBinarizer()
+    labels_vecs.fit_transform(labels)
+    return labels_vecs
+    
+def one_hot_encoder_np(x):
+    """   
+    : x: List of sample Labels
+    : return:Numpy array of one-hot encoded labels
+    """
+	import numpy as np
+    return np.eye(number_of_labels)[x]
