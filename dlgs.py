4b021318f5f26b5f937749dfc3dcb7a8158e1fa6
George Studenko <gnstudenko@gmail.com>
2017-07-28 11:00:41 +0200

    bug: Add tensorflow import statement

--- dlgs.py
+++ dlgs.py
@@ -1,30 +1,31 @@
 def create_tf_checkpoint(sess, path, version = 1):
-	"""
+    """
     Creates a tensoflow checkpoint 
     : sess: The TensorFlow session you want to save
     : path: The name of the file to save the session to
     : version: The version of the checkpoint, by default is 1, so you can create different checkpoints in time for your model    
     """  
-	saver = tf.train.Saver()
-	save_path = saver.save(sess, path, global_stet = version)
+    saver = tf.train.Saver()
+    save_path = saver.save(sess, path, global_stet = version)
 
 def restore_tf_checkpoint(sess, path):
-	loader = tf.train.import_meta_graph(path + '.meta')
-	loader.restore(sess, path)		
-		
-def get_tf_weights(shape, stddev = 0.1):	
-	weights = tf.Variable(tf.truncated_normal(shape=shape,stddev = stddev))
-	return weights
+    loader = tf.train.import_meta_graph(path + '.meta')
+    loader.restore(sess, path)
+
+def get_tf_weights(shape, stddev = 0.1):
+    import tensorflow as tf
+    weights = tf.Variable(tf.truncated_normal(shape=shape,stddev = stddev))
+    return weights
 
 def get_tf_biases(num_outputs):
-	biases = tf.Variable(tf.zeros(num_outputs))
-	return biases
+    biases = tf.Variable(tf.zeros(num_outputs))
+    return biases
 
-	
+
 def get_bag_of_words(text):
-	from collections import Counter
-	return Counter(text.split())
-	
+    from collections import Counter
+    return Counter(text.split())
+
 def normalize_image(x):
     """
     Normalize a list of sample image data in the range of 0 to 1
@@ -42,7 +43,7 @@ def normalize_image(x):
         ( grayscale_max - grayscale_min ))       
     
     return normalized_x
-	
+
 def one_hot_encode_sk(x):
     """   
     : x: List of sample Labels
@@ -59,5 +60,5 @@ def one_hot_encoder_np(x):
     : x: List of sample Labels
     : return:Numpy array of one-hot encoded labels
     """
-	import numpy as np
-	return np.eye(number_of_labels)[x]
+    import numpy as np
+    return np.eye(number_of_labels)[x]
